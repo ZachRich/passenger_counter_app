f903c27f398c23ab17b00c6b62679b26bce42d6b
ZachRich <ZachRichDev@gmail.com>
2022-02-26 17:17:36 -0500

    Added Save button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 let count = 0;
 let count_element = document.getElementById("count-el");
+let num = document.getElementById("save-el"); 
+
 
 function increment() {
 
@@ -8,3 +10,14 @@ function increment() {
     count_element.innerHTML = count;
 
 }
+
+function save() {
+
+    let numstr = count + " - ";
+    num.textContent += numstr
+    console.log(count)
+    count_element.textContent = 0;
+    count = 0;
+
+}
+
